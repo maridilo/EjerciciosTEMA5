df7676cb3ccb9ec0ad8c74c90e639150584cd31e
maridilo <146822054+maridilo@users.noreply.github.com>
2024-04-20 00:24:06 +0200

    commit

--- Ejercicios/src/main/java/Main.java
+++ Ejercicios/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.*;
+
 public class Main {
     public static void main(String[] args) {
+        // Nuevo Frame
+        JFrame frame = new JFrame("Suma numeros naturales");
 
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el numero hasta el cual desea calcular la suma de los numeros naturales");
+        int n = scanner.nextInt();
+        // Llamada a la funcion sumNaturales
+        int suma = ej1.sumNaturales(n);
+        System.out.println("La suma de los primeros " + n + "numeros naturales es: " + suma);
+        scanner.close();
     }
 }
